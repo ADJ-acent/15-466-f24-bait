3dfc0e169124e10cd8b30f704e5582e48638f7b3
Andy Jiang <weiwenj@andrew.cmu.edu>
2024-11-26 12:52:12 -0500

    fixe virtual destructor

--- Particles.hpp
+++ Particles.hpp
@@ -30,6 +30,8 @@ struct ParticleSystem
         Particle() = default;
         Particle(float total_lifetime_, glm::vec2 scale_, glm::vec3 position_, glm::vec3 velocity_, glm::vec3 tint_ = glm::vec3(1)) : 
             total_lifetime(total_lifetime_), scale(scale_), base_scale(scale_), position(position_), velocity(velocity_), tint(tint_) {};
+
+        virtual ~Particle() = default;
     };
 
     struct Bubble : Particle {
@@ -38,6 +40,8 @@ struct ParticleSystem
         virtual bool update(float elapsed, const Scene::Camera &active_camera) override;
         Bubble(float total_lifetime_, glm::vec2 scale_, glm::vec3 position_, glm::vec3 velocity_, glm::vec3 tint_ = glm::vec3(1)) : 
             Particle(total_lifetime_, scale_, position_, velocity_, tint_) {};
+
+        virtual ~Bubble() = default;
     };
     //many particles share the same textures, can bind the texture just once with the unordered map
     // the key is casted GLuint
