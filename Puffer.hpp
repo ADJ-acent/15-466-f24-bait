25deb46de94f2307730989b3f67d859c4fea8a3a
Charvi Hoysal <choysal@andrew.cmu.edu>
2024-12-03 16:22:23 -0500

    changed speed back

--- Puffer.hpp
+++ Puffer.hpp
@@ -90,7 +90,7 @@ struct Puffer {
     inline static constexpr float puffer_velocity_halflife = .3f;
     inline static constexpr float puffer_rotation_return_halflife = 0.1f;
     inline static constexpr float puffer_rotation_release_halflife = .3f;
-    inline static constexpr float speed = 3.0f;
+    inline static constexpr float speed = 1.0f;
     inline static constexpr float gravity = 1.0f;
     inline static constexpr float oxygen_down_speed = 5.0f;
     inline static constexpr float oxygen_up_speed = 10.0f;
